0ef9eacb648f70a530743f39da3e135b679f25ca
Roi Martin <jroi.martin@gmail.com>
2024-09-13 09:10:26 +0200

    src/lib.rs: implement regular expression emulator

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,7 @@
 //! alternation (`|`), then concatenation, and finally the repetition
 //! operators (`*`, `+` and `?`).
 
-use std::{fmt, iter, slice};
-
-/// Parsing error.
-#[derive(Debug, Eq, PartialEq)]
-pub enum ParsingError {
-    UnmatchedParenthesis,
-    UnexpectedToken(Token),
-    Eof,
-}
+use std::{cell::RefCell, fmt, iter, rc::Rc, slice, str};
 
 /// Lexical token.
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
@@ -63,18 +55,33 @@ pub fn scan(regexp: &str) -> Vec<Token> {
     tokens
 }
 
+/// Parsing error.
+#[derive(Debug, Eq, PartialEq)]
+pub enum ParsingError {
+    UnmatchedParenthesis,
+    UnexpectedToken(Token),
+    Eof,
+}
+
 /// The Abstract Syntax Tree (AST) of a regular expression.
 ///
-/// Implements following grammar (from weakest to strongest binding):
+/// Implements the following grammar (from weakest to strongest
+/// binding):
 ///
+/// ```text
 /// alternation   = concatenation {PIPE concatenation} .
 /// concatenation = repetition {CONCAT repetition} .
 /// repetition    = grouping [STAR | PLUS | QUESTION] .
 /// grouping      = LEFT_PAREN alternation RIGHT_PARENT | matching .
 /// matching      = CHAR .
+/// ```
 ///
 /// The alternation and concatenation operators are left-associative.
-#[derive(Debug, Eq, PartialEq)]
+///
+/// Note that the `CONCAT` token is not present in the string
+/// representation of the regular expression.  It is added by the
+/// lexical scanner to represent two adjacent expressions.
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub enum Expr {
     Alternation { lhs: Box<Expr>, rhs: Box<Expr> },
     Concatenation { lhs: Box<Expr>, rhs: Box<Expr> },
@@ -85,7 +92,7 @@ pub enum Expr {
 
 /// Number of times the inner expression of an [`Expr::Repetition`]
 /// expression is repeated.
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub enum Times {
     ZeroOrMore,
     OneOrMore,
@@ -114,27 +121,27 @@ impl fmt::Display for Expr {
     }
 }
 
-/// Performs the syntactic analysis the provided tokens.  It returns
-/// the parsed AST.
+/// Performs the syntactic analysis of the provided tokens.  It
+/// returns the parsed AST.
 pub fn parse(tokens: &[Token]) -> Result<Expr, ParsingError> {
     Parser::parse(tokens)
 }
 
-/// Represents a parser.
+/// Stores the state of the regular expression parser.
 struct Parser<'a> {
     /// Token peekable iterator.
-    iter: iter::Peekable<slice::Iter<'a, Token>>,
+    tokens: iter::Peekable<slice::Iter<'a, Token>>,
 }
 
 impl Parser<'_> {
-    /// Performs the syntactic analysis the provided tokens.  It
+    /// Performs the syntactic analysis of the provided tokens.  It
     /// returns the parsed AST.
     fn parse(tokens: &[Token]) -> Result<Expr, ParsingError> {
         let mut parser = Parser {
-            iter: tokens.iter().peekable(),
+            tokens: tokens.iter().peekable(),
         };
         let expr = parser.parse_alternation()?;
-        match parser.iter.next() {
+        match parser.tokens.next() {
             Some(tok) => Err(ParsingError::UnexpectedToken(*tok)),
             None => Ok(expr),
         }
@@ -185,7 +192,7 @@ impl Parser<'_> {
     fn parse_grouping(&mut self) -> Result<Expr, ParsingError> {
         if self.check(Token::LeftParen) {
             let expr = self.parse_alternation()?;
-            return match self.iter.next() {
+            return match self.tokens.next() {
                 Some(Token::RightParen) => Ok(Expr::Grouping(Box::new(expr))),
                 _ => Err(ParsingError::UnmatchedParenthesis),
             };
@@ -195,7 +202,7 @@ impl Parser<'_> {
 
     /// Parses a matching expression.
     fn parse_matching(&mut self) -> Result<Expr, ParsingError> {
-        match self.iter.next() {
+        match self.tokens.next() {
             Some(Token::Char(ch)) => Ok(Expr::Matching(*ch)),
             Some(tok) => Err(ParsingError::UnexpectedToken(*tok)),
             None => Err(ParsingError::Eof),
@@ -205,7 +212,132 @@ impl Parser<'_> {
     /// Matches the next token against the passed token.  In the case
     /// of a match, it returns true and advances the iterator cursor.
     fn check(&mut self, tok: Token) -> bool {
-        self.iter.next_if_eq(&&tok).is_some()
+        self.tokens.next_if_eq(&&tok).is_some()
+    }
+}
+
+/// Stores the state of the regular expression emulator.
+pub struct Emulator {
+    /// String to match.
+    s: String,
+
+    /// Current states of the emulator.
+    states: Vec<(Rc<RefCell<State>>, usize)>,
+}
+
+/// A state of the internal non-deterministic finite automata (NFA).
+#[derive(Default)]
+struct State {
+    /// Character to match.
+    ch: Option<char>,
+
+    /// Links to next states.
+    fwd: Vec<Rc<RefCell<State>>>,
+}
+
+impl Emulator {
+    /// Returns a new [`Emulator`].
+    pub fn new(expr: &Expr, s: &str) -> Emulator {
+        let (start, _) = Emulator::compile(expr);
+        Emulator {
+            s: s.into(),
+            states: vec![(start, 0)],
+        }
+    }
+
+    /// Creates the internal NFA.  It returns the start and end states
+    /// of the provided expression.
+    fn compile(expr: &Expr) -> (Rc<RefCell<State>>, Rc<RefCell<State>>) {
+        match expr {
+            Expr::Alternation { lhs, rhs } => {
+                let split = Rc::new(RefCell::new(State::default()));
+                let join = Rc::new(RefCell::new(State::default()));
+                let (lhs_start, lhs_end) = Self::compile(lhs);
+                let (rhs_start, rhs_end) = Self::compile(rhs);
+                split.borrow_mut().fwd = vec![lhs_start, rhs_start];
+                lhs_end.borrow_mut().fwd = vec![Rc::clone(&join)];
+                rhs_end.borrow_mut().fwd = vec![Rc::clone(&join)];
+                (split, join)
+            }
+            Expr::Concatenation { lhs, rhs } => {
+                let (lhs_start, lhs_end) = Self::compile(lhs);
+                let (rhs_start, rhs_end) = Self::compile(rhs);
+                lhs_end.borrow_mut().fwd = vec![rhs_start];
+                (lhs_start, rhs_end)
+            }
+            Expr::Repetition(expr, times) => {
+                let (expr_start, expr_end) = Self::compile(expr);
+                let join = Rc::new(RefCell::new(State::default()));
+                match times {
+                    Times::ZeroOrOne => {
+                        let split = Rc::new(RefCell::new(State::default()));
+                        split.borrow_mut().fwd = vec![expr_start, Rc::clone(&join)];
+                        expr_end.borrow_mut().fwd = vec![Rc::clone(&join)];
+                        (split, join)
+                    }
+                    Times::ZeroOrMore => {
+                        let split = Rc::new(RefCell::new(State::default()));
+                        split.borrow_mut().fwd = vec![expr_start, Rc::clone(&join)];
+                        expr_end.borrow_mut().fwd = vec![Rc::clone(&join), Rc::clone(&split)];
+                        (split, join)
+                    }
+                    Times::OneOrMore => {
+                        expr_end.borrow_mut().fwd = vec![Rc::clone(&join), Rc::clone(&expr_start)];
+                        (expr_start, join)
+                    }
+                }
+            }
+            Expr::Grouping(expr) => Self::compile(expr),
+            Expr::Matching(ch) => {
+                let expr = Rc::new(RefCell::new(State {
+                    ch: Some(*ch),
+                    ..State::default()
+                }));
+                (Rc::clone(&expr), expr)
+            }
+        }
+    }
+
+    /// Emulates one step forward.
+    pub fn step(&mut self) -> Option<bool> {
+        if self.states.is_empty() {
+            return Some(false);
+        }
+
+        let mut new_states = Vec::new();
+        for (state, mut idx) in &self.states {
+            let state = state.borrow();
+            if let Some(ch) = state.ch {
+                let Some(sch) = self.s.chars().nth(idx) else {
+                    continue;
+                };
+                if ch != sch {
+                    continue;
+                }
+                idx += 1;
+            }
+            if state.fwd.is_empty() {
+                if idx == self.s.len() {
+                    return Some(true);
+                }
+                continue;
+            }
+            for fwd in &state.fwd {
+                new_states.push((Rc::clone(fwd), idx))
+            }
+        }
+        self.states = new_states;
+
+        None
+    }
+
+    /// Emulates the regular expression.
+    pub fn emulate(&mut self) -> bool {
+        loop {
+            if let Some(res) = self.step() {
+                return res;
+            }
+        }
     }
 }
 
@@ -291,7 +423,7 @@ mod tests {
                 }),
                 rhs: Box::new(Expr::Matching('c')),
             })
-        )
+        );
     }
 
     #[test]
@@ -308,7 +440,7 @@ mod tests {
                     rhs: Box::new(Expr::Matching('d')),
                 }))),
             })
-        )
+        );
     }
 
     #[test]
@@ -334,7 +466,7 @@ mod tests {
                 }),
                 rhs: Box::new(Expr::Matching('d')),
             })
-        )
+        );
     }
 
     #[test]
@@ -342,7 +474,7 @@ mod tests {
         assert_eq!(
             parse(&scan("((a|b)|c")),
             Err(ParsingError::UnmatchedParenthesis)
-        )
+        );
     }
 
     #[test]
@@ -350,7 +482,7 @@ mod tests {
         assert_eq!(
             parse(&scan("a++")),
             Err(ParsingError::UnexpectedToken(Token::Plus))
-        )
+        );
     }
 
     #[test]
@@ -358,19 +490,76 @@ mod tests {
         assert_eq!(
             parse(&scan("|a")),
             Err(ParsingError::UnexpectedToken(Token::Pipe))
-        )
+        );
     }
 
     #[test]
     fn parse_error_eof() {
-        assert_eq!(parse(&scan("a|")), Err(ParsingError::Eof))
+        assert_eq!(parse(&scan("a|")), Err(ParsingError::Eof));
     }
 
     #[test]
-    fn ast_to_string() {
+    fn expr_to_string() {
         assert_eq!(
             parse(&scan("a+(b?|c)*d")).unwrap().to_string(),
             "a+(b?|c)*d"
-        )
+        );
+    }
+
+    #[test]
+    fn emulator_emulate() {
+        for (re, s, res) in &[
+            ("a", "a", true),
+            ("a", "b", false),
+            ("abc", "abc", true),
+            ("abc", "abd", false),
+            ("a|b", "a", true),
+            ("a|b", "b", true),
+            ("a|b", "c", false),
+            ("a|b|c", "a", true),
+            ("a|b|c", "b", true),
+            ("a|b|c", "c", true),
+            ("a|b|c", "d", false),
+            ("(a|b)c", "ac", true),
+            ("(a|b)c", "bc", true),
+            ("(a|b)c", "cc", false),
+            ("(a|b)c", "aa", false),
+            ("a?", "", true),
+            ("a?", "a", true),
+            ("a?", "b", false),
+            ("a?b", "ab", true),
+            ("a?b", "b", true),
+            ("a?b", "a", false),
+            ("a*", "a", true),
+            ("a*", "aa", true),
+            ("a*", "", true),
+            ("a*", "b", false),
+            ("a*a", "a", true),
+            ("a*a", "aaaaa", true),
+            ("a*b", "aaabb", false),
+            ("a*b", "aaaab", true),
+            ("a*b", "b", true),
+            ("a+", "a", true),
+            ("a+", "aa", true),
+            ("a+", "", false),
+            ("a+b", "ab", true),
+            ("a+b", "aab", true),
+            ("a+a", "a", false),
+            ("a+b", "b", false),
+            ("a+(b?|c)*d", "aabbd", true),
+            ("a+(b?|c)*d", "ad", true),
+            ("a+(b?|c)*d", "accd", true),
+            ("a+(b?|c)*d", "cd", false),
+            ("a+(b?|c)*d", "bd", false),
+            // ("a+(b?|c)*d", "a", false), // Infinite loop.
+            ("(a?)*b", "ab", true),
+            ("(a?)*b", "b", true),
+            // ("(a?)*b", "", false), // Infinite loop.
+            // ("(a*)*b", "", false), // Infinite loop.
+            // ("(a?)+b", "", false), // Infinite loop.
+            // ("(a*)+b", "", false), // Infinite loop.
+        ] {
+            assert_eq!(Emulator::new(&parse(&scan(re)).unwrap(), s).emulate(), *res);
+        }
     }
 }
